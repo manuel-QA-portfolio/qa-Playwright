f2b6c00520b67ea700bc137c948b7282c985235f
Manuel López <manulm91@proton.me>
2025-09-17 13:11:02 +0200

    Update playwright.config.ts to fix pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,15 +4,27 @@ export default defineConfig({
   testDir: './tests/swaglabs',
   retries: 1,
   reporter: [['html', { open: 'never' }], ['line']],
+  fullyParallel: true, 
   use: {
-    baseURL: 'https://www.saucedemo.com',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure'
   },
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'SauceDemo',
+      testDir: './tests/swaglabs',
+      use: {
+        baseURL: 'https://www.saucedemo.com',
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'TheInternet',
+      testDir: './tests/theInternet',
+      use: {
+        baseURL: 'https://theinternet.io',
+        ...devices['Desktop Chrome'],
+      },
     },
   ],
 });
